f6e9d751472490c3fde85b1097a00527e9a90ef8
rjmcbain <rmcbain01@gmail.com>
2018-05-01 11:18:32 -0600

    adding profile schema

--- models/Profile.js
+++ models/Profile.js
@@ -42,6 +42,54 @@ const ProfileSchema = new Schema({
         required: true
       },
       company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
         type: String
       }
     }
