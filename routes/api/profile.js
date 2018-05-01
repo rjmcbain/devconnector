c81a61aadf0458ac31b10a606e8aab0bce3c438b
rjmcbain <rmcbain01@gmail.com>
2018-05-01 13:58:36 -0600

    adding experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -167,4 +167,30 @@ router.post(
   }
 );
 
+// @route           GET api/profile/experience
+// @description     Add experience to profile
+// @access          Private
+
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+      // Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
 module.exports = router;
