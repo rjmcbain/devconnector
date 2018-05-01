ef7b0f496ed45eb423fb29083a10d26ce855803c
rjmcbain <rmcbain01@gmail.com>
2018-05-01 11:11:06 -0600

    adding profile Schema

--- models/Profile.js
+++ models/Profile.js
@@ -2,4 +2,48 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
-const ProfileSchema = new Schema({});
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 50
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String
+      }
+    }
+  ]
+});
