9c4e169b0d1bba770233dfe0a5a994bf061b8b3a
rjmcbain <rmcbain01@gmail.com>
2018-05-01 12:11:30 -0600

    updating profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -37,4 +37,34 @@ router.get(
   }
 );
 
+// @route           POST api/profile
+// @description     Create user profile
+// @access          Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    // Skills - Split into array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+  }
+);
+
 module.exports = router;
