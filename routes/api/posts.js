680b2062153e4c7bd5c9d77f4446118752cd716a
rjmcbain <rmcbain01@gmail.com>
2018-05-02 14:21:33 -0600

    adding posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Post model
 const Post = require("../../models/Post");
+
+// Validation
+const validatePostInput = require("../../validation/post");
 
 // @route           GET api/posts/test
 // @description     Tests post route
@@ -19,6 +23,14 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
