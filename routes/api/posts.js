3ddb424034696d1ffd8c7c591dbe09089e1fb365
rjmcbain <rmcbain01@gmail.com>
2018-05-02 16:52:53 -0600

    adding comments section

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -119,7 +119,7 @@ router.post(
 
           post.save().then(post => res.json(post));
         })
-        .catch(err => res.status(404).json({ postnotfound: "Nopost found" }));
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
     });
   }
 );
@@ -155,9 +155,34 @@ router.post(
           // Save
           post.save().then(post => res.json(post));
         })
-        .catch(err => res.status(404).json({ postnotfound: "Nopost found" }));
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
     });
   }
 );
+
+// @route           POST api/posts/comment/:id
+// @description     Add comment to post
+// @access          Private
+
+router.post("/comment/:id"),
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  };
 
 module.exports = router;
