8fa4f967769390c18c8f20ce49a05eefd9028b4a
rjmcbain <rmcbain01@gmail.com>
2018-05-02 16:40:07 -0600

    adding ability to delete posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -71,6 +71,7 @@ router.post(
 // @route           DELETE api/posts/:id
 // @description     DELETE Posts
 // @access          Private
+
 router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
@@ -87,6 +88,36 @@ router.delete(
 
           // Delete
           post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "Nopost found" }));
+    });
+  }
+);
+
+// @route           POST api/posts/like/:id
+// @description     Like Posts
+// @access          Private
+
+router.post(
+  "like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          }
+
+          // Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
         })
         .catch(err => res.status(404).json({ postnotfound: "Nopost found" }));
     });
