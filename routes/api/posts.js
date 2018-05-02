a4d30dafde0d6b3931a37ee2d82b07629818c4bf
rjmcbain <rmcbain01@gmail.com>
2018-05-02 15:44:36 -0600

    adding posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,6 +15,29 @@ const validatePostInput = require("../../validation/post");
 
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
+// @route           GET api/posts
+// @description     GET Posts
+// @access          Public
+
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// @route           GET api/posts/:id
+// @description     GET Posts by id
+// @access          Public
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
+
 // @route           POST api/posts
 // @description     Create Posts
 // @access          Private
@@ -34,7 +57,7 @@ router.post(
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
-      avatar: req.body.name,
+      avatar: req.body.avatar,
       user: req.user.id
     });
 
