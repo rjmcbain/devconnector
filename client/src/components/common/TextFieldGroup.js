17861597a36d4dc74fc009ff9ba8ea39540dac00
rjmcbain <rmcbain01@gmail.com>
2018-05-06 17:05:39 -0600

    adding text field group

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -1,6 +1,6 @@
 import React from "react";
-import classNames from "classnames";
-import propTypes from "prop-types";
+import classnames from "classnames";
+import PropTypes from "prop-types";
 
 const TextFieldGroup = ({
   name,
