636f0dfeec9cd739f83cea0b1eef70d0fe3dfcae
rjmcbain <rmcbain01@gmail.com>
2018-05-06 15:37:07 -0600

    adding logout

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -55,4 +55,6 @@ export const logoutUser = () => dispatch => {
   localStorage.removeItem("jwtToken");
   // Remove auth header for future requests
   setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
 };
