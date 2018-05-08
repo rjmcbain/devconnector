eb9aa438dd973ff0ae563614705af0fa816a1a71
rjmcbain <rmcbain01@gmail.com>
2018-05-07 18:29:56 -0600

    adding create profile

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+class CreateProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaySocialInputs: false,
+      handle: "",
+      company: "",
+      website: "",
+      location: "",
+      status: "",
+      skills: "",
+      githubusername: "",
+      bio: "",
+      twitter: "",
+      facebook: "",
+      linkedin: "",
+      youtube: "",
+      instagram: "",
+      errors: {}
+    };
+  }
+  render() {
+    return <div />;
+  }
+}
+
+export default connect(null)(CreateProfile);
