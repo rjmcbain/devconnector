10147dd174ffb24b467cfa2976f8f145288c4c9d
rjmcbain <rmcbain01@gmail.com>
2018-05-07 20:45:59 -0600

    adding create profile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import TextFieldGroup from "../common/TextFieldGroup";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -24,8 +25,32 @@ class CreateProfile extends Component {
     };
   }
   render() {
-    return <div />;
+    return (
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">
+                Let's get some information to make your profile stand out
+              </p>
+              <small className="d-block pb-3">* = required fields</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default connect(null)(CreateProfile);
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps)(CreateProfile);
