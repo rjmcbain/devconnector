8de81431de6212a53c6de8c5b7e7cdb7890bfbc3
rjmcbain <rmcbain01@gmail.com>
2018-05-14 15:48:03 -0600

    adding create profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   // GET_ERRORS,
-  CLEAR_CURRENT_PROFILE
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS
 } from "./types";
 
 // GET current profile
@@ -22,6 +23,19 @@ export const getCurrentProfile = () => dispatch => {
       dispatch({
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       })
     );
 };
