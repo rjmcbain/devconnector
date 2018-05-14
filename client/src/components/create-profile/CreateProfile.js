5942923ff5fbda424e1c62adf8a7aeadc569e390
rjmcbain <rmcbain01@gmail.com>
2018-05-14 16:01:38 -0600

    adding create profile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -203,6 +203,7 @@ class CreateProfile extends Component {
                 />
                 <div className="mb-3">
                   <button
+                    type="button"
                     onClick={() => {
                       this.setState(prevState => ({
                         displaySocialInputs: !prevState.displaySocialInputs
