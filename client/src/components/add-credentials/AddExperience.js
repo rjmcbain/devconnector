1f6e218ca2189d71fbf9a4ab38331820f08b2753
rjmcbain <rmcbain01@gmail.com>
2018-07-09 21:35:25 -0600

    adding experience

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -34,6 +34,52 @@ class AddExperience extends Component {
                     <h1 className="display-4 text-center">Add Experience</h1>
                     <p className="lead text-center">Add any job or position that you have had in the past or current</p>
                     <small className="d-block pb-3">* = required fields</small>
+                    <form onSubmit={this.onSubmit}>
+                        <TextFieldGroup
+                            placeholder="* Company"
+                            name="company"
+                            value={this.state.company}
+                            onChange={this.onChange}
+                            error={errors.company}
+                        />
+                            <TextFieldGroup
+                            placeholder="* Job Title"
+                            name="title"
+                            value={this.state.title}
+                            onChange={this.onChange}
+                            error={errors.title}
+                        />
+                        <h6>From Date</h6>
+                        <TextFieldGroup
+                            name="from"
+                            type="date"
+                            value={this.state.from}
+                            onChange={this.onChange}
+                            error={errors.from}
+                        />
+                        <h6>To Date</h6>
+                        <TextFieldGroup
+                            name="to"
+                            type="date"
+                            value={this.state.to}
+                            onChange={this.onChange}
+                            error={errors.to}
+                            disabled={this.state.disabled ? 'disabled' : ""}
+                        />
+                        <div className="form-check mb-4">
+                            <input type="checkbox"
+                                className="form-check-input"
+                                name="current"
+                                value={this.state.current}
+                                checked={this.state.current}
+                                onChange={this.onCheck}
+                                id="current"
+                            />
+                            <label htmlFor="current" className="form-check-label">
+                                Current Job
+                            </label>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
