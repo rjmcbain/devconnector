211e04ae75019eb106a4753dbb323efbd06ec68f
rjmcbain <rmcbain01@gmail.com>
2018-07-09 19:12:45 -0600

    adding experience

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -7,6 +7,20 @@ import { PropTypes } from 'prop-types';
 
 
 class AddExperience extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            company: '',
+            title: '',
+            location: '',
+            from: '',
+            to: '',
+            current: false,
+            description: '',
+            erros: {},
+            disabled: false
+        }
+    }
   render() {
     return (
       <div>
