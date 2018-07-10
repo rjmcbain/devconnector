2583bf792339db4d0b67da256f89065c70557800
rjmcbain <rmcbain01@gmail.com>
2018-07-09 19:11:06 -0600

    adding experience

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -1,4 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { TextFieldGroup } from '../common/TextFieldGroup';
+import { TextAreaFieldGroup } from '../common/TextAreaFieldGroup';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+
 
 class AddExperience extends Component {
   render() {
