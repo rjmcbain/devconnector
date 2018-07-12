fc01eee1df61d72957eff04418ea6614fc3e84a0
rjmcbain <rmcbain01@gmail.com>
2018-07-11 18:51:47 -0600

    adding education

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -6,6 +6,9 @@ import Moment from 'react-moment';
 import { deleteExperience } from '../../actions/profileActions';
 
 class Experience extends Component {
+    onDeleteClick(id) {
+        this.props.deleteExperience(id, this.props.history);
+    }
   render() {
       const experience = this.props.experience.map(exp => (
           <tr key={exp._id}>
@@ -14,7 +17,7 @@ class Experience extends Component {
             <td>
                         <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{exp.to === null ? (' Now') : (<Moment format="YYYY/MM/DD">{exp.to}</Moment>)}
                     </td>
-            <td><button className="btn btn-danger">Delete</button></td>
+            <td><button onClick= {this.onDeleteClick.bind(this, exp._id)} className="btn btn-danger">Delete</button></td>
           </tr>
       ))
     return (
@@ -36,4 +39,8 @@ class Experience extends Component {
   }
 }
 
-export default connect(null)(withRouter(Experience));
+Experience.propTypes = {
+    deleteExperience: PropTypes.func.isRequired
+}
+
+export default connect(null, { deleteExperience })(withRouter(Experience));
