fa9ac30b1161a8ebe59abe59afdb360567663386
rjmcbain <rmcbain01@gmail.com>
2018-07-11 19:28:16 -0600

    adding experience and education

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
-import { deleteExperience } from '../../actions/profileActions';
+import { deleteEducation } from '../../actions/profileActions';
 
-class Experience extends Component {
+class Education extends Component {
     onDeleteClick(id) {
-        this.props.deleteExperience(id);
+        this.props.deleteEducation(id);
     }
   render() {
-      const experience = this.props.experience.map(exp => (
+      const education = this.props.education.map(exp => (
           <tr key={exp._id}>
             <td>{exp.company}</td>
             <td>{exp.title}</td>
@@ -21,7 +21,7 @@ class Experience extends Component {
       ))
     return (
       <div>
-        <h4 className="mb-4">Experience Credentials</h4>
+        <h4 className="mb-4">Education Credentials</h4>
         <table className="table">
             <thead>
                 <tr>
@@ -30,7 +30,7 @@ class Experience extends Component {
                     <th>Years</th>
                     <th />
                 </tr>
-                    {experience}
+                    {education}
             </thead>
         </table>
       </div>
@@ -38,8 +38,8 @@ class Experience extends Component {
   }
 }
 
-Experience.propTypes = {
-    deleteExperience: PropTypes.func.isRequired
+Education.propTypes = {
+    deleteEducation: PropTypes.func.isRequired
 }
 
-export default connect(null, { deleteExperience })(Experience);
+export default connect(null, { deleteEducation })(Education);
