0784b1796496897d984543fa141e0acfe9a09246
rjmcbain <rmcbain01@gmail.com>
2018-07-15 13:36:44 -0600

    adding profile items

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -18,11 +18,32 @@ class ProfileItem extends Component {
                 <p>
                     {profile.status} {isEmpty(profile.company) ? null : (<span>at {profile.company}</span>)}
                 </p>
+                <p>
+                    {isEmpty(profile.location) ? null : (<span>{profile.location}</span>)}
+                </p>
+                <Link to={`/profile/${profile.handle}`} className="btn btn-info">
+                    View Profile
+                </Link>
             </div>    
+            <div className="col-md-4 d-none d-md-block">
+                <h4>Skill Set</h4>
+                <ul className="list-group">
+                    {profile.skills.slice(0, 4).map((skill, inex) => (
+                        <li key={index} className="list-group-item">
+                            <i className="fa fa-check pr-1"/>
+                            {skill}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
       </div>
     )
   }
+}
+
+ProfileItem.propTypes = {
+    profile: PropTypes.object.isRequired
 }
 
 export default ProfileItem;
