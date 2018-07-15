78d1b540071d298d804b0f7071b86550c7c17e1b
rjmcbain <rmcbain01@gmail.com>
2018-07-15 14:35:37 -0600

    adding profile items

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,8 +7,14 @@ import ProfileAbout from './ProfileAbout';
 import ProfileCreds from './ProfileCreds';
 import ProfileGithub from './ProfileGithub';
 import Spinner from '../common/Spinner';
+import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
+    componentDidMount() {
+        if(this.props.match.params.handle) {
+            this.props.getProfileByHandle(this.props.match.params.handle);
+        }
+    }
   render() {
     return (
       <div>
