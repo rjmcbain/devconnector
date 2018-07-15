9d79047c9618a48aa3e0b4d7ee3c4372825e14f9
rjmcbain <rmcbain01@gmail.com>
2018-07-15 14:00:05 -0600

    adding profile items

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ProfileHeader from './ProfileHeader';
+import ProfileAbout from './ProfileAbout';
+import ProfileCreds from './ProfileCreds';
+import ProfileGithub from './ProfileGithub';
+import Spinner from '../common/Spinner';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Profile;
