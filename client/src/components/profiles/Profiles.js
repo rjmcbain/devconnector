996feba94109ee752b2d01ce6dbb64a51b92f168
rjmcbain <rmcbain01@gmail.com>
2018-07-15 12:45:09 -0600

    adding profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -10,9 +10,31 @@ class Profiles extends Component {
   }
 
   render() {
-    const {  }
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
+
+    if(profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if(profiles.length > 0) {
+        <h1>Profiles Here</h1>
+      } else {
+        profileItems = <h4>No Profiles Found...</h4>
+      }
+    }
+
     return (
-      <div>
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <p className="lead text-center">
+                Browse and connect with developers
+              </p>
+            </div>
+          </div>
+        </div>
         
       </div>
     )
