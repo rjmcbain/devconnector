792cc0798b37444288085165a7492c9fb6b7b148
rjmcbain <rmcbain01@gmail.com>
2018-07-15 16:30:43 -0600

    securing keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://rj:rj@ds239387.mlab.com:39387/devconnectors",
-  secretOrKey: "secret"
-};
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
