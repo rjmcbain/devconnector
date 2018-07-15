98ae11179ee9e22a111250335d922859b06d2600
rjmcbain <rmcbain01@gmail.com>
2018-07-15 14:02:20 -0600

    adding profile items

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ProfileHeader from './ProfileHeader';
 import ProfileAbout from './ProfileAbout';
 import ProfileCreds from './ProfileCreds';
@@ -15,4 +18,12 @@ class Profile extends Component {
   }
 }
 
-export default Profile;
+Profile.propTypes = {
+    profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    profile: state.profile
+});
+
+export default connect(mapStateToProps)(Profile);
